61501ae0b14b16f450e9a2f0d24b5dbc00fc1f96
ewakur <ewa.kurczab13@gmail.com>
2020-10-14 14:23:49 +0200

    Updates

---
+++ precache-manifest.32246cda329de3167eec994ebb5580e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5185e7a7b40655c866d238783465208",
+    "url": "/Single-page-application/index.html"
+  },
+  {
+    "revision": "2820911b05362c110bd6",
+    "url": "/Single-page-application/static/css/main.bcdd0fa8.chunk.css"
+  },
+  {
+    "revision": "e247aedd0ddb2421efe1",
+    "url": "/Single-page-application/static/js/2.a18e560e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Single-page-application/static/js/2.a18e560e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2820911b05362c110bd6",
+    "url": "/Single-page-application/static/js/main.d07a1d81.chunk.js"
+  },
+  {
+    "revision": "a7c891bbdee5136dfee9",
+    "url": "/Single-page-application/static/js/runtime-main.46798980.js"
+  },
+  {
+    "revision": "3732bb28725362c4f98e9131a290ae64",
+    "url": "/Single-page-application/static/media/panorama-5stawow.3732bb28.JPG"
+  },
+  {
+    "revision": "4f14b7d06128a3e99f233671cb910e89",
+    "url": "/Single-page-application/static/media/panorama.4f14b7d0.JPG"
+  },
+  {
+    "revision": "9d07720d25312c0bc83b5c2b7ab809a7",
+    "url": "/Single-page-application/static/media/piecstawow.9d07720d.jpg"
+  },
+  {
+    "revision": "357b39d466e1b287b34236f2eb62ab6b",
+    "url": "/Single-page-application/static/media/wejscie5stawow.357b39d4.jpg"
+  }
+]);
